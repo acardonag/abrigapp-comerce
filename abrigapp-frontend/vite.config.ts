121dc7e71cd9796baa9d9dff09bebc600ee871ee
Augusto Cardona Giraldo <augusto.cardona@hotmail.com>
2026-08-14 21:59:21 -0500

    Configure Vite multi-page

--- abrigapp-frontend/vite.config.ts
+++ abrigapp-frontend/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        tienda: resolve(__dirname, 'tienda.html')
+      }
+    }
+  },
   server: {
     port: 5173,
     proxy: {
